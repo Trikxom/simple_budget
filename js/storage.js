ca76090e4ceadd89a2b8e765b45ad22b6c191f17
Kevin Dunleavy <kevdunleavy@gmail.com>
2012-09-19 18:52:12 +0100

    Deleted old files.
    They were no longer being used since jQuery rewrite.

--- js/storage.js
+++
@@ -1,9 +0,0 @@
-function save(element_to_save) {
-    console.log("storing: ", element_to_save);
-    localStorage.setItem("budget_items", element_to_save.innerHTML);
-}
-
-function load_budget_items() {
-    console.log("loading old budget_items from local storage");
-    return localStorage.getItem("budget_items");
-}
